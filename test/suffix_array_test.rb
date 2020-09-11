301eac3a1047827ffa96df7e4e104ec3fe3a6efc
kotatsugame <takaryou1019@gmail.com>
2020-09-12 06:07:37 +0900

    add suffix_array, lcp_array, suffix_array_test

---
+++ test/suffix_array_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'minitest'
+require 'minitest/autorun'
+
+require_relative '../src/suffix_array.rb'
+
+def suffix_array_naive(s)
+  s = s.bytes if s === String
+  n = s.size
+  return (0 ... n).sort_by{ |i| s[i..-1] }
+end
+
+class SuffixArrayTest < Minitest::Test
+  def test_random_array_small_elements
+    maxA = 10
+    20.times{
+      a = (0 ... 100).map{ rand(maxA) }
+      assert_equal suffix_array(a), suffix_array_naive(a)
+    }
+  end
+
+  def test_random_array_big_elements
+    maxA = 10**18
+    20.times{
+      a = (0 ... 100).map{ rand(maxA) }
+      assert_equal suffix_array(a), suffix_array_naive(a)
+    }
+  end
+
+  def test_random_array_given_upper
+    maxA = 100
+    20.times{
+      a = (0 ... 100).map{ rand(maxA + 1) }
+      assert_equal suffix_array(a, maxA), suffix_array_naive(a)
+    }
+  end
+
+  def test_random_string
+    maxA = ?~.ord - ?\s.ord + 1
+    20.times{
+      s = (0 ... 100).map{ (rand(maxA) + ?\s.ord).chr }.join
+      assert_equal suffix_array(s), suffix_array_naive(s)
+    }
+  end
+
+  def test_mississippi
+    assert_equal suffix_array("mississippi"), suffix_array_naive("mississippi")
+  end
+end
