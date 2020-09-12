1c519faa35015cd1132dafa2c8a17df37e224c8a
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-09-13 00:11:56 +0900

    Add Priority Queue
    Reference: https://github.com/python/cpython/blob/master/Lib/heapq.py
    
    There is not a priority queue implemantation in Ruby.
    This priority queue is based on an array heap.

---
+++ test/example/priority_queue.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../../src/priority_queue.rb'
+
+# Warning: This sample is a solution of the following problem:
+#   https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_9_C
+
+q = PriorityQueue.new
+ans = []
+
+loop do
+  input = gets.chomp
+  case input
+  when 'end'
+    break
+  when 'extract'
+    ans << q.pop
+  else
+    v = input.split[1].to_i
+    q.push(v)
+  end
+end
+
+puts ans
---
+++ test/priority_queue_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'minitest'
+require 'minitest/autorun'
+
+require_relative '../src/priority_queue.rb'
+
+class PriorityQueueTest < Minitest::Test
+  # https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_9_C
+  def test_aizu_sample_case
+    q = PriorityQueue.new([2, 7, 8])
+    assert_equal 8, q.get
+    assert_equal 8, q.pop
+    q.push(19)
+    q.push(10)
+    assert_equal 19, q.pop
+    assert_equal 10, q.pop
+    q.append(8)
+    assert_equal 8, q.pop
+    assert_equal 7, q.pop
+    q.<< 3
+    q.push(4)
+    q.push(1)
+    assert_equal 4, q.pop
+    assert_equal [3, 2, 1], q.heap
+    assert_equal 3, q.pop
+    assert_equal 2, q.pop
+    assert_equal 1, q.pop
+    assert_equal true, q.empty?
+    assert_equal nil, q.top
+  end
+
+  def test_asc_order
+    q = HeapQueue.new { |x, y| x < y }
+    q << 2
+    q << 3
+    q << 1
+    assert_equal 1, q.pop
+    assert_equal 2, q.pop
+    assert_equal 3, q.pop
+  end
+end
