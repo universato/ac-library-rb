e51790e2d576d8f0b70ed31364dc64e2208b58c5
kotatsugame <takaryou1019@gmail.com>
2020-09-13 08:31:02 +0900

    change implementation of z_algorithm

--- src/z_algorithm.rb
+++ src/z_algorithm.rb
@@ -1,16 +1,31 @@
 # frozen_string_literal: true
 
+# this implementation is different from ACL because of calculation time
+# ref : https://snuke.hatenablog.com/entry/2014/12/03/214243
+# ACL  implementation : https://atcoder.jp/contests/abc135/submissions/16656972 (1320 ms)
+# this implementation : https://atcoder.jp/contests/abc135/submissions/16656965 (1076 ms)
+
 def z_algorithm(s)
   n = s.size
   return [] if n == 0
-
+ 
   z = [0]*n
-  j = 0
-  1.upto(n-1){ |i|
-    z[i] = j+z[j] <= i ? 0 : j+z[j]-i < z[i-j] ? j+z[j]-i : z[i-j]
-    z[i] += 1 while i+k<n && s[z[i]]==s[i+z[i]]
-    j = i if j+z[j] < i+z[i]
-  }
   z[0] = n
+  i, j = 1, 0
+  while i < n
+    j += 1 while i+j<n && s[j]==s[i+j]
+    z[i] = j
+    if j == 0 then
+      i += 1
+      next
+    end
+    k = 1
+    while i+k<n && k+z[k]<j
+      z[i+k] = z[k]
+      k += 1
+    end
+    i += k
+    j -= k
+  end
   return z
 end
