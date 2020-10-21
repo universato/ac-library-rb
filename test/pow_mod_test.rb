2548eda1e9dac6900a1792ced23f51e8b08bf125
Yoshimine Sato <universato@gmail.com>
2020-10-21 13:57:33 +0900

    add pow_mod

---
+++ test/pow_mod_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'minitest'
+require 'minitest/autorun'
+
+require_relative '../lib/pow_mod.rb'
+
+def naive_pow_mod(x, n, mod)
+  y = x % mod
+  z = 1 % mod
+  n.times { z = (z * y) % mod }
+  z
+end
+
+class InvModTest < Minitest::Test
+  def test_prime_mod
+    (-10 .. 10).each do |a|
+      (0 .. 10).each do |b|
+        (2 .. 10).each do |c|
+          assert_equal naive_pow_mod(a, b, c), pow_mod(a, b, c)
+          # assert_equal naive_pow_mod(a, b, c), a.pow(b, c)
+        end
+      end
+    end
+  end
+end
